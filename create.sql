e5789f6b110198cb33920d4e995f246d365fb522
Sam Heidler <123268532+SamH135@users.noreply.github.com>
2023-11-04 18:18:05 -0500

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,51 +1,68 @@
+
+-- Create the Inventory table
+CREATE TABLE Inventory (
+    ProductID INT,
+    ProductName VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (ProductID, ProductName),
+    FOREIGN KEY (ProductID, ProductName) REFERENCES Product(ProductID, Name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
 -- Create the User table
 CREATE TABLE User (
     UserID INT PRIMARY KEY,
-    JobTitle VARCHAR(255) NOT NULL,
-    Username VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL
+    JobTitle VARCHAR(255),
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Password VARCHAR(255)
 );
 
--- Create the Supplier table
-CREATE TABLE Supplier (
-    SupplierID INT PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    Phone VARCHAR(20),
+-- Create the Manufacturer table
+CREATE TABLE Manufacturer (
+    ManufacturerID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Phone VARCHAR(255),
     Email VARCHAR(255)
 );
 
--- Create the Product table
+-- Create the Product table with a composite primary key
 CREATE TABLE Product (
-    ProductID INT PRIMARY KEY,
-    Price DECIMAL(10, 2) NOT NULL,
+    ProductID INT,
+    Name VARCHAR(255),
+    Price DECIMAL(10, 2),
     Description TEXT,
-    Name VARCHAR(255) NOT NULL
+    PRIMARY KEY (ProductID, Name)
 );
 
--- Create the Supplies table with a composite primary key
+-- Create the Supplies table with three primary keys
 CREATE TABLE Supplies (
     ProductID INT,
-    SupplierID INT,
-    PRIMARY KEY (ProductID, SupplierID),
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID) ON DELETE CASCADE,
-    FOREIGN KEY (SupplierID) REFERENCES Supplier(SupplierID) ON DELETE CASCADE
+    ProductName VARCHAR(255),
+    ManufacturerID INT,
+    PRIMARY KEY (ProductID, ProductName, ManufacturerID),
+    FOREIGN KEY (ProductID, ProductName) REFERENCES Product(ProductID, Name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
--- Create the Order table
-CREATE TABLE Orders (
+-- Create the Order table with ON DELETE SET NULL
+CREATE TABLE `Order` (
     OrderID INT PRIMARY KEY,
-    Description TEXT,
     Date DATE,
     Quantity INT,
     UserID INT,
     ProductID INT,
-    FOREIGN KEY (UserID) REFERENCES User(UserID) ON DELETE CASCADE,
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID) ON DELETE CASCADE
+    FOREIGN KEY (UserID) REFERENCES `User`(UserID)
+        ON DELETE SET NULL      -- so we don't lose records if a user is deleted 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
--- Create the Inventory table
-CREATE TABLE Inventory (
-    ProductID INT PRIMARY KEY,
-    ProductName VARCHAR(255) NOT NULL,
-    FOREIGN KEY (ProductID) REFERENCES Product(ProductID) ON DELETE CASCADE
-);
